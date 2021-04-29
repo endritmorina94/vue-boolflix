ddd5e88afa43f55549fe6bf5eb7926c86da52599
endritmorina94 <endrit.morina@outlook.it>
2021-04-30 01:32:14 +0200

    finito milestone 1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            key: "99d1efa101b179fd70c80c694373d975",
+            query: "rambo",
             movies: []
         },
         methods: {
+            find() {
+
+            }
         },
         mounted() {
             axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=99d1efa101b179fd70c80c694373d975&query=Rambo')
+                .get(`https://api.themoviedb.org/3/search/movie?api_key=${this.key}&query=${this.query}`)
                 .then((response) => {
                     const result = response.data.results;
                     console.log(result);
