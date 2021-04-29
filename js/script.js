107e4a0d5e62ad7d0e2bafac5718d2f7884a96cd
endritmorina94 <endrit.morina@outlook.it>
2021-04-30 00:52:18 +0200

    primi passi con API..

--- js/script.js
+++ js/script.js
@@ -2,10 +2,18 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            movies: []
         },
         methods: {
         },
-        mounted() {        
+        mounted() {
+            axios
+                .get('https://api.themoviedb.org/3/search/movie?api_key=99d1efa101b179fd70c80c694373d975&query=Rambo')
+                .then((response) => {
+                    const result = response.data.results;
+                    console.log(result);
+                    this.movies = result;
+                });
         }
     }
 );
